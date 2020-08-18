d0ec729558c2028f2280d8a77be94368113df040
Gklam <khalid.lamsadi@outlook.fr>
2020-08-18 02:30:01 +0100

    Updates

---
+++ precache-manifest.78df82bb580e6dd7cb65a6b0ab6766ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d40f58746d3223205a0f35e0f310b829",
+    "url": "/Random-Movie/index.html"
+  },
+  {
+    "revision": "14d45870fd8bf9ff2c1a",
+    "url": "/Random-Movie/static/css/main.f96df29c.chunk.css"
+  },
+  {
+    "revision": "bf52239df8868e706dfa",
+    "url": "/Random-Movie/static/js/2.2cca5344.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Random-Movie/static/js/2.2cca5344.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14d45870fd8bf9ff2c1a",
+    "url": "/Random-Movie/static/js/main.10948436.chunk.js"
+  },
+  {
+    "revision": "abeeb213b301c8da2787",
+    "url": "/Random-Movie/static/js/runtime-main.0e78dcb5.js"
+  }
+]);
